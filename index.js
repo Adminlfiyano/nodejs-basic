7ad26f541520bfff8fdb4005e5713f641fae7f25
Alfiyanor Madani <alfimadani@gmail.com>
2022-08-07 19:43:05 +0800

    menambahakan require dan menambahkan function fightting

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
-const initialMemoryUsage = process.memoryUsage().heapUsed;
-const yourName = process.argv[2];
-const environment = process.env.NODE_ENV;
-
-for(let i = 0; i <= 10000; i++) {
+// const initialMemoryUsage = process.memoryUsage().heapUsed;
+// const yourName = process.argv[2];
+// const environment = process.env.NODE_ENV;
+// 
+// for(let i = 0; i <= 10000; i++) {
   // Proses looping ini akan membuat penggunaan memori naik
+// }
+// 
+// const currentMemoryUsage = process.memoryUsage().heapUsed;
+// 
+// console.log(`Hai, ${yourName}`);
+// console.log(`Mode environment: ${environment}`)
+// console.log(`Penggunaan memori dari ${initialMemoryUsage} naik ke ${currentMemoryUsage}`);
+
+const Wolf = require('./modularization/Wolf')
+const Tiger = require('./modularization/Tiger')
+
+const fightting = (tiger, wolf) => {
+  if (tiger.stringth > wolf.stringth){
+    tiger.growl();
+    return;
+  }
+  if (wolf.stringth > tiger.stringth){
+    wolf.howl();
+    return;
+  }
 }
-
-const currentMemoryUsage = process.memoryUsage().heapUsed;
-
-console.log(`Hai, ${yourName}`);
-console.log(`Mode environment: ${environment}`)
-console.log(`Penggunaan memori dari ${initialMemoryUsage} naik ke ${currentMemoryUsage}`);
