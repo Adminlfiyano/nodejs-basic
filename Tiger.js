5df1ffb3d94afcc5e025cb5bd05e5cc0b23122a3
Alfiyanor Madani <alfimadani@gmail.com>
2022-08-07 19:23:14 +0800

    Menambahan class dan consturctur dalam file dan export module

---
+++ Tiger.js
@@ -0,0 +1,10 @@
+class Tiger {
+    constructor(){
+        this.stringth = Math.floor(Math.random()*100);
+    }
+    growl(){
+        console.log('grrrr!')
+    }
+}
+
+module.exports = Tiger
---
+++ Wolf.js
@@ -0,0 +1,11 @@
+class Wolf {
+    constructor(){
+        this.stringth = Math.floor(Math.random() * 100);
+    }
+
+    howl(){
+        console.log('owoooo!');
+    }
+}
+
+module.exports = Wolf;
